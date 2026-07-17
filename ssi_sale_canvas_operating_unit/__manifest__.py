1bc35d3350515dc77280468f38efb9d73948b449
Andhitia Rama <andhitia.r@gmail.com>
2026-07-17 19:56:37 +0700

    [ADD] ssi_sale_canvas_operating_unit
    Modul glue baru yang menambahkan integrasi Operating Unit pada model
    sale_canvas (mewarisi mixin.single_operating_unit), lengkap dengan
    field operating_unit_id, security (res.groups + ir.rule), dan
    integrasi view (tree/form/search).

---
+++ ssi_sale_canvas_operating_unit/__manifest__.py
@@ -0,0 +1,23 @@
+# Copyright 2026 OpenSynergy Indonesia
+# Copyright 2026 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Sale Canvas + Operating Unit",
+    "version": "14.0.1.0.0",
+    "website": "https://simetri-sinergi.id",
+    "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
+    "contributors": [
+        "Andhitia Rama <andhitia.r@gmail.com>",
+    ],
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "ssi_sale_canvas",
+        "ssi_operating_unit_mixin",
+    ],
+    "data": [
+        "security/res_group/sale_canvas.xml",
+        "security/ir_rule/sale_canvas.xml",
+        "view/sale_canvas.xml",
+    ],
+}
---
+++ ssi_sale_canvas_operating_unit/readme/DESCRIPTION.rst
@@ -0,0 +1,3 @@
+Glue module that adds Operating Unit support to the Sale Canvas module.
+Extends ``sale_canvas`` with ``mixin.single_operating_unit``, adding the
+``operating_unit_id`` field, security groups/rules, and view integration.
